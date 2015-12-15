7c19181f4c0967a149bdb6fb1140c596f7a03a73
IM Hoang Thai <hoangthai95ne@gmail.com>
2015-12-15 10:59:33 +0700

    Initial Commit

---
+++ runningLed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  runningLed
+//
+//  Created by Hoàng Thái on 12/15/15.
+//  Copyright © 2015 HOANGTHAI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ runningLedTests/runningLedTests.m
@@ -0,0 +1,39 @@
+//
+//  runningLedTests.m
+//  runningLedTests
+//
+//  Created by Hoàng Thái on 12/15/15.
+//  Copyright © 2015 HOANGTHAI. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface runningLedTests : XCTestCase
+
+@end
+
+@implementation runningLedTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
